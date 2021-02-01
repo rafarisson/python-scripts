a09d95d3fe59cd4b4519ff53210b912132f512b8
Rafael Risson <rafaelrisson@xpert.com.br>
2021-02-01 17:02:01 -0300

    Tag to filename

--- mp3-format-cover-type.py
+++ mp3-format-cover-type.py
@@ -11,6 +11,7 @@ REMOVE_TAG_ID3V1 = True
 REMOVE_TAG_GENRE = True
 REMOVE_TAG_ALBUM = True
 REMOVE_TAG_ALBUM_ARTIST = True
+CONVERT_TAG_TO_FILENAME = True
 
 proc = {}
 
@@ -18,7 +19,7 @@ t0 = time.time()
 for path, subdirs, files in os.walk(os.getcwd()):
 	for name in files:
 		if os.path.isfile(os.path.join(path, name)) and name.lower().endswith(".mp3"):
-			proc[name] = 'wait'
+			status = { 'status': 'W', 'error': None, 'name': '' }
 			tempname = 'mytemp.mp3'
 			try:
 				os.chdir(path)
@@ -36,26 +37,36 @@ for path, subdirs, files in os.walk(os.getcwd()):
 					if REMOVE_TAG_ALBUM_ARTIST:
 						audio.tag.album_artist = None
 					audio.tag.save()
-					if audio.tag.images != None:
+					if audio.tag.images != None and len(audio.tag.images) > 0:
 						description = audio.tag.images[0].description
 						removed = audio.tag.images.remove(description)
 						if removed.picture_type != eyed3.id3.frames.ImageFrame.OTHER:
 							audio.tag.images.set(0, removed.image_data, removed.mime_type, description)
 							audio.tag.save()
-							proc[name] = 'converted'
+							status['status'] = 'C'
 						else:
-							proc[name] = 'ignored'
+							status['status'] = 'I'
 					else:
-						proc[name] = 'no image'
-			except:
-				proc[name] = 'error'
-			os.rename(tempname, name)
+						status['status'] = 'N'
+					status['name'] = name;
+					if CONVERT_TAG_TO_FILENAME:
+						status['name'] = audio.tag.title + ' - ' + audio.tag.artist + '.mp3'
+						if name != status['name']:
+							status['status'] += 'R'
+					os.rename(tempname, status['name'])
+			except Exception as e:
+				status['status'] = 'E'
+				status['error'] = str(e)
+			proc[name] = status
 t0 = time.time()-t0
 
 print('--------------------------------------')
 print('total time:', t0)
 print('total files:', len(proc))
 for k in proc:
-	print('  {:12s}{:s}'.format(proc[k], k))
+	e = ''
+	if proc[k]['error'] != None:
+		e = '** ' + proc[k]['error']
+	print('  {:5s}{:70s} {:s}'.format(proc[k]['status'], proc[k]['name'], e))
 print('--------------------------------------')
 
