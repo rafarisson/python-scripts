080412c74d7588616e65f0145af0c7c77ddf45d7
Rafael Risson <rafaelrisson@xpert.com.br>
2021-04-27 15:24:16 -0300

    Usable version

--- mp3-format-cover-type.py
+++ mp3-format-cover-type.py
@@ -3,72 +3,49 @@
 
 # pip install eyed3
 
-import os
+import argparse
 import eyed3
-import time
-import re
+import pathlib
 
-REMOVE_TAG_ID3V1 = True
-REMOVE_TAG_GENRE = True
-REMOVE_TAG_ALBUM = True
-REMOVE_TAG_ALBUM_ARTIST = True
-CONVERT_TAG_TO_FILENAME = True
+parser = argparse.ArgumentParser(description='mp3 tags for Renault navmedia')
+parser.add_argument('files',
+					action='store',
+					nargs='+',
+					help='mp3 files')
+parser.add_argument('-t', 
+					action='store_true',
+					dest='preserve_filename',
+					help='preserve file name (disable convert tag \'title - artist\' to file name)')
+parser.add_argument('-p',
+					action='store_true',
+					dest='preserve_id3v1',
+					help='preserve id3v1 tags')
 
-proc = {}
+args = parser.parse_args();
 
-t0 = time.time()
-for path, subdirs, files in os.walk(os.getcwd()):
-	for name in files:
-		if os.path.isfile(os.path.join(path, name)) and name.lower().endswith(".mp3"):
-			status = { 'status': 'W', 'error': None, 'name': '' }
-			tempname = 'mytemp.mp3'
-			try:
-				os.chdir(path)
-				if os.path.exists(tempname):
-					os.remove(tempname)
-				os.rename(name, tempname)
-				audio = eyed3.load(tempname)
-				if audio != None:
-					if REMOVE_TAG_ID3V1:
-						audio.tag.remove(tempname, version=eyed3.id3.ID3_V1, preserve_file_time=True)
-					if REMOVE_TAG_GENRE:
-						audio.tag.genre = None
-					if REMOVE_TAG_ALBUM:
-						audio.tag.album = None
-					if REMOVE_TAG_ALBUM_ARTIST:
-						audio.tag.album_artist = None
-					audio.tag.save()
-					if audio.tag.images != None and len(audio.tag.images) > 0:
-						description = audio.tag.images[0].description
-						removed = audio.tag.images.remove(description)
-						if removed.picture_type != eyed3.id3.frames.ImageFrame.OTHER:
-							audio.tag.images.set(0, removed.image_data, removed.mime_type, description)
-							audio.tag.save()
-							status['status'] = 'C'
-						else:
-							status['status'] = 'I'
-					else:
-						status['status'] = 'N'
-					status['name'] = name;
-					if CONVERT_TAG_TO_FILENAME:
-						status['name'] = audio.tag.title + ' - ' + audio.tag.artist + '.mp3'
-						status['name'] = re.sub(r'[/|\\]', r'', status['name'])
-						if name != status['name']:
-							status['status'] += 'R'
-					os.rename(tempname, status['name'])
-			except Exception as e:
-				status['status'] = 'E'
-				status['error'] = str(e)
-			proc[name] = status
-t0 = time.time()-t0
+# on exception, see terminal messages
+wait_input = False;
 
-print('--------------------------------------')
-print('total time:', t0)
-print('total files:', len(proc))
-for k in proc:
-	e = ''
-	if proc[k]['error'] != None:
-		e = '** ' + proc[k]['error']
-	print('  {:5s}{:70s} {:s}'.format(proc[k]['status'], proc[k]['name'], e))
-print('--------------------------------------')
+for f in args.files:
+	try:
+		audio = eyed3.load(f)
+		if audio is not None:
+			if not args.preserve_id3v1:
+				audio.tag.remove(f, version=eyed3.id3.ID3_V1, preserve_file_time=True)
+			if audio.tag.images is not None and len(audio.tag.images) > 0:
+				for i in audio.tag.images:
+					if i.picture_type is not eyed3.id3.frames.ImageFrame.OTHER:
+						i.picture_type = eyed3.id3.frames.ImageFrame.OTHER
+			audio.tag.save(preserve_file_time=True)
+			if not args.preserve_filename:
+				tag_filename = audio.tag.title + ' - ' + audio.tag.artist
+				if pathlib.Path(f).stem != tag_filename:
+					audio.rename(tag_filename, preserve_file_time=True);
+		print(f + ' -> ok');
+	except Exception as e:
+		wait_input = True
+		print(f + ' -> error: ' + str(e))
 
+# no exception, bye
+if wait_input:
+	input()
