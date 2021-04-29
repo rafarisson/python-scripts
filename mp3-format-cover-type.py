1529a979d698e906b2bea1c4cfe052f5a491a505
Rafael Risson <rafaelrisson@xpert.com.br>
2021-04-29 09:35:58 -0300

    Support recursive path

--- mp3-format-cover-type.py
+++ mp3-format-cover-type.py
@@ -5,13 +5,14 @@
 
 import argparse
 import eyed3
-import pathlib
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='mp3 tags for Renault navmedia')
-parser.add_argument('files',
+parser.add_argument('path',
+					nargs='?',
 					action='store',
-					nargs='+',
-					help='mp3 files')
+					default='.',
+					help='path or mp3 files')
 parser.add_argument('-p',
 					action='store_true',
 					dest='preserve_id3v1',
@@ -22,27 +23,32 @@ args = parser.parse_args();
 # on exception, see terminal messages
 wait_input = False;
 
-for f in args.files:
-	try:
-		audio = eyed3.load(f)
-		if audio is not None:
-			if not args.preserve_id3v1:
-				audio.tag.remove(f, version=eyed3.id3.ID3_V1, preserve_file_time=True)
-			if audio.tag.images is not None and len(audio.tag.images) > 0:
-				for i in audio.tag.images:
-					i.description = ''
-					if i.picture_type is not eyed3.id3.frames.ImageFrame.OTHER:
-						i.picture_type = eyed3.id3.frames.ImageFrame.OTHER
-			audio.tag.album_artist = None
-			audio.tag.genre = None
-			audio.tag.save(preserve_file_time=True)
-			tag_filename = audio.tag.title + ' - ' + audio.tag.artist
-			if pathlib.Path(f).stem != tag_filename:
-				audio.rename(tag_filename, preserve_file_time=True);
-		print(f + ' -> ok');
-	except Exception as e:
-		wait_input = True
-		print(f + ' -> error: ' + str(e))
+for p in args.path:
+	p = Path(p)
+	if not p.exists():
+		continue
+	files = (p.is_file() and p.exists()) and [p] or p.glob('**/*.mp3')
+	for f in files:
+		try:
+			audio = eyed3.load(f)
+			if audio is not None:
+				if not args.preserve_id3v1:
+					audio.tag.remove(f, version=eyed3.id3.ID3_V1, preserve_file_time=True)
+				if audio.tag.images is not None and len(audio.tag.images) > 0:
+					for i in audio.tag.images:
+						i.description = ''
+						if i.picture_type is not eyed3.id3.frames.ImageFrame.OTHER:
+							i.picture_type = eyed3.id3.frames.ImageFrame.OTHER
+				audio.tag.album_artist = None
+				audio.tag.genre = None
+				audio.tag.save(preserve_file_time=True)
+				tag_filename = audio.tag.title + ' - ' + audio.tag.artist
+				if Path(f).stem != tag_filename:
+					audio.rename(tag_filename, preserve_file_time=True);
+			print('{0} -> ok'.format(f));
+		except Exception as e:
+			wait_input = True
+			print('{0} -> error: {1}'.format(f, str(e)))
 
 # no exception, bye
 if wait_input:
